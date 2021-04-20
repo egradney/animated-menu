f2f27fef72065db2fcf27ee15f360a01e19e290f
Elli <elligradney@gmail.com>
2021-04-19 22:24:42 -0500

    refactord JS

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
 
 function toggleNav() {
     // Toggle: Menu Bars Open/Closed
@@ -16,37 +17,21 @@ function toggleNav() {
         overlay.classList.add('overlay-slide-right'); 
         overlay.classList.remove('overlay-slide-left');
         // Animate In - NavItems
-        nav1.classList.remove('slide-out-1');
-        nav1.classList.add('slide-in-1');
-        nav2.classList.remove('slide-out-2');
-        nav2.classList.add('slide-in-2');
-        nav3.classList.remove('slide-out-3');
-        nav3.classList.add('slide-in-3');
-        nav4.classList.remove('slide-out-4');
-        nav4.classList.add('slide-in-4');
-        nav5.classList.remove('slide-out-5');
-        nav5.classList.add('slide-in-5');
+        navItems.forEach( item => {
+            item.classList.remove(`slide-out-${navItems.indexOf(item) + 1}`);
+            item.classList.add(`slide-in-${navItems.indexOf(item) + 1}`)
+        });
     } else {
             // Animate Out - Overlay
         overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
         // Animate Out - NavItems
-        nav1.classList.remove('slide-in-1');
-        nav1.classList.add('slide-out-1');
-        nav2.classList.remove('slide-in-2');
-        nav2.classList.add('slide-out-2');
-        nav3.classList.remove('slide-in-3');
-        nav3.classList.add('slide-out-3');
-        nav4.classList.remove('slide-in-4');
-        nav4.classList.add('slide-out-4');
-        nav5.classList.remove('slide-in-5');
-        nav5.classList.add('slide-out-5');
+        navItems.forEach( item => {
+            item.classList.remove(`slide-in-${navItems.indexOf(item) + 1}`);
+            item.classList.add(`slide-out-${navItems.indexOf(item) + 1}`)
+        });
     }
 }
 
 // Event Listeners
 menuBars.addEventListener('click', toggleNav);
-nav1.addEventListener('click', toggleNav);
-nav2.addEventListener('click', toggleNav);
-nav3.addEventListener('click', toggleNav);
-nav4.addEventListener('click', toggleNav);
-nav5.addEventListener('click', toggleNav);
+navItems.forEach( item => {item.addEventListener('click', toggleNav)});
